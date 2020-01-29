b3cbab7b1ae3d4bdd881b0a2eec8cfb77018ea37
Mr_DarkBladeS <MrLamaScura@gmail.com>
2020-01-29 20:49:52 +0200

    Fix Numbers

--- Java_Lessons/Lesson14 - STO Tests/Honda.java
+++
@@ -1,8 +0,0 @@
-
-public class Honda extends Car {
-	public Honda() {
-		this.rpm = (int) ((Math.random() * 4000 + 3000));
-		this.durability = 0.5 * Math.random();
-		this.numberFaults = (int) (0.5 * (Math.random() * 7 + 3));
-	}
-}
